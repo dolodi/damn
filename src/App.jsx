99e9f94a12510131857f328ee6d46d5be06976c0
lluaminha <support@luaminha.com>
2024-03-17 02:42:56 +0100

    Update page title and animation for Valentine's Day

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Lottie from 'lottie-react'
-import animationData from './assets/Animation-piramid.json'
+import animationData from './assets/valentines.json'
 
 
 function App() {
@@ -14,15 +14,18 @@ function App() {
       
       <h1>Want to be my valentine?????</h1>
       <div className="card">
-        
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => setCount((count) => count + 1)} >
+          yes 
+        </button>
+        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => setCount((count) => count + 1)} >
+          no 
+        </button>
         <div class="w-100 h-100 flex justify-center items-center">
           <Lottie animationData={animationData} />
         </div>
       </div>
       
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more oTEST
-      </p>
+      
      
     </>
   )
